f92d868b9ac1ebecca866b4afaf7cb9233890bfa
adofsauron <adofsauron@163.com>
2022-05-06 23:07:31 +0800

    fix main

--- aufs/aufs.c
+++ aufs/aufs.c
@@ -6,13 +6,13 @@
 #include <linux/cred.h>
 #include <linux/mount.h>
 
-//每个文件系统需要一个MAGIC number
+// 每个文件系统需要一个MAGIC number
 #define AUFS_MAGIC  0x64668735
 
-//aufs文件系统的挂载点
+// aufs文件系统的挂载点
 static struct vfsmount *aufs_mount;
 
-//根据创建的aufs文件系统的 super_block创建具体的inode结构体
+// 根据创建的aufs文件系统的 super_block创建具体的inode结构体
 static struct inode *aufs_get_inode(struct super_block *sb, int mode, dev_t dev)
 {
     struct inode *inode = new_inode(sb);
@@ -43,7 +43,7 @@ static struct inode *aufs_get_inode(struct super_block *sb, int mode, dev_t dev)
     return inode;
 }
 
-//把创建的inode和dentry结构体连接起来
+// 把创建的inode和dentry结构体连接起来
 static int aufs_mknod(struct inode *dir, struct dentry *dentry, int mode, dev_t dev)
 {
     struct inode * inode;
@@ -78,7 +78,7 @@ static int aufs_create(struct inode *dir, struct dentry *dentry, int mode)
     return aufs_mknod(dir, dentry, mode | S_IFREG, 0);
 }
 
-//根据父dentry、mode、name创建子dentry
+// 根据父dentry、mode、name创建子dentry
 static int aufs_create_by_name(const char *name, mode_t mode,
         struct dentry *parent, struct dentry **dentry)
 {
@@ -108,7 +108,7 @@ static int aufs_create_by_name(const char *name, mode_t mode,
     return error;
 }
 
-//在aufs文件系统中创建文件
+// 在aufs文件系统中创建文件
 struct dentry *aufs_create_file(const char *name, mode_t mode,
             struct dentry *parent, void *data,
             struct file_operations *fops)
@@ -134,7 +134,7 @@ exit:
     return dentry;
 }
 
-//在aufs文件系统中创建一个文件夹
+// 在aufs文件系统中创建一个文件夹
 struct dentry *aufs_create_dir(const char *name, struct dentry *parent)
 {
     return aufs_create_file(name, S_IFDIR | S_IRWXU | S_IRUGO, parent, NULL, NULL);
@@ -142,7 +142,7 @@ struct dentry *aufs_create_dir(const char *name, struct dentry *parent)
  
 static int enabled = 1;
 
-//对应于打开的aufs文件的读取方法
+// 对应于打开的aufs文件的读取方法
 static ssize_t aufs_file_read(struct file *fle, char __user *buf, size_t nbytes, loff_t *ppos)
 {
     char *s = enabled ? "aufs read enabled\\n" : "aufs read disabled\\n";
@@ -150,7 +150,7 @@ static ssize_t aufs_file_read(struct file *fle, char __user *buf, size_t nbytes,
     return simple_read_from_buffer(buf, nbytes, ppos, s, strlen(s));
 }
 
-//对应于打开的aufs文件的写入方法
+// 对应于打开的aufs文件的写入方法
 static ssize_t aufs_file_write(struct file *file, const char __user *buffer, size_t count, loff_t *ppos)
 {
     int res = *buffer - '0';
@@ -163,13 +163,13 @@ static ssize_t aufs_file_write(struct file *file, const char __user *buffer, siz
     return count;
 }
 
-//对应具体打开文件的文件操作方式
+// 对应具体打开文件的文件操作方式
 static struct file_operations aufs_file_operations = {
     .read = aufs_file_read,
     .write = aufs_file_write,
 };
 
-//用于填充aufs的super_block
+// 用于填充aufs的super_block
 static int aufs_fill_super(struct super_block *sb, void *data, int silent)
 {
     static struct tree_descr debug_files[] = {{""}};
@@ -177,21 +177,21 @@ static int aufs_fill_super(struct super_block *sb, void *data, int silent)
     return simple_fill_super(sb, AUFS_MAGIC, debug_files);
 }
 
-//创建aufs文件系统的对应的根目录的dentry
+// 创建aufs文件系统的对应的根目录的dentry
 static struct dentry *aufs_get_sb(struct file_system_type *fs_type,
         int flags, const char *dev_name, void *data)
 {
     return mount_single(fs_type, flags, data, aufs_fill_super);
 }
 
-//初始化aufs文件系统的 file_system_type结构，每个文件系统对应一个这样的结构体，主要用于提供具体文件系统的//的信息，以及操作的方法
+// 初始化aufs文件系统的 file_system_type结构，每个文件系统对应一个这样的结构体，主要用于提供具体文件系统的// 的信息，以及操作的方法
 static struct file_system_type aufs_type = {
     .name = "aufs",
     .mount = aufs_get_sb,
     .kill_sb = kill_litter_super,
 };
 
-//创建aufs文件系统，同时创建对应的文件夹和文件
+// 创建aufs文件系统，同时创建对应的文件夹和文件
 static int __init aufs_init(void)
 {
     int ret;
@@ -211,8 +211,8 @@ static int __init aufs_init(void)
         return ret;
     }
  
-    pslot = aufs_create_dir("woman_star", NULL); //创建woman_star文件系统，返回所创建文件夹的dentry
-    aufs_create_file("lbb", S_IFREG | S_IRUGO, pslot, NULL, &aufs_file_operations);//在对应的文件夹下，创建具体的文件
+    pslot = aufs_create_dir("woman_star", NULL); // 创建woman_star文件系统，返回所创建文件夹的dentry
+    aufs_create_file("lbb", S_IFREG | S_IRUGO, pslot, NULL, &aufs_file_operations);// 在对应的文件夹下，创建具体的文件
     aufs_create_file("fbb", S_IFREG | S_IRUGO, pslot, NULL, &aufs_file_operations);
     aufs_create_file("lj1", S_IFREG | S_IRUGO, pslot, NULL, &aufs_file_operations);
  
@@ -224,7 +224,7 @@ static int __init aufs_init(void)
     return 0;
 }
 
-//卸载aufs文件系统
+// 卸载aufs文件系统
 static void __exit aufs_exit(void)
 {
     kern_unmount(aufs_mount);
